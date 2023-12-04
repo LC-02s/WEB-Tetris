a01d3a15fb19114e4c7b21201932e15fdb7324df
LC-02s <cl700120@gmail.com>
2023-12-04 12:44:19 +0900

    divide preloader

--- app/src/public/js/ui_common.js
+++ app/src/public/js/ui_common.js
@@ -3,70 +3,6 @@
 
 import playSoundEffect from "./ui_sound.js";
 
-// intro
-const preloadAssetArr = [
-  '../img/asset/asset_block_gray.svg',
-  '../img/asset/asset_block_red.svg',
-  '../img/asset/asset_block_orange.svg',
-  '../img/asset/asset_block_yellow.svg',
-  '../img/asset/asset_block_green.svg',
-  '../img/asset/asset_block_cyan.svg',
-  '../img/asset/asset_block_blue.svg',
-  '../img/asset/asset_block_purple.svg',
-  '../img/asset/asset_preview_gray.svg',
-  '../img/asset/asset_preview_red.svg',
-  '../img/asset/asset_preview_orange.svg',
-  '../img/asset/asset_preview_yellow.svg',
-  '../img/asset/asset_preview_green.svg',
-  '../img/asset/asset_preview_cyan.svg',
-  '../img/asset/asset_preview_blue.svg',
-  '../img/asset/asset_preview_purple.svg',
-];
-
-function preloadAsset(assetArr) {
-  const setAsset = [];
-  const progressEl = document.getElementById('preload-progress');
-  const totalProgressNum = assetArr.length;
-  let countProgressNum = 0;
-
-  function updateProgressBar() {
-    const progress = Math.floor((countProgressNum / totalProgressNum) * 100);
-    const progressCipher = String(progress).padStart(2, '0');
-    progressEl.innerText = `Loading... ${progressCipher}%`;
-    console.log(`${progressCipher}%`);
-  }
-
-  for (let i = 0; i < totalProgressNum; i += 1) {
-    const [, assetName] = assetArr[i].match(/asset_([^/]+)\.svg$/);
-    setAsset[i] = new Image();
-    setAsset[i].id = assetName;
-    setAsset[i].src = assetArr[i];
-
-    /* eslint-disable-next-line */
-    setAsset[i].onload = () => {
-      countProgressNum += 1;
-      if (countProgressNum === totalProgressNum) {
-        progressEl.innerText = 'Press To Start Game';
-        progressEl.classList.add('loaded');
-      } else {
-        updateProgressBar();
-      }
-    };
-    setAsset[i].onerror = () => {
-      progressEl.innerText = 'Error Please Reload';
-      progressEl.classList.add('error');
-      console.error('Image not loaded');
-    };
-  }
-
-  return setAsset;
-}
-
-const assetList = preloadAsset(preloadAssetArr);
-
-// console.log(assetList);
-
-
 const introContainer = document.getElementById('intro-container');
 const progressState = document.getElementById('preload-progress');
 const menuContainer = document.getElementById('menu-container');
@@ -226,7 +162,3 @@ vsAiModeBtn.addEventListener('click', () => {
   // menuContainer.dataset.disabled = 0;
   // gameContainer.dataset.disabled = 1;
 });
-
-export default {
-  assetList
-}
