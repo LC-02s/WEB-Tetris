dc9aa1551e4fae9386db4731dff1a4da8e63d4e5
LC-02s <cl700120@gmail.com>
2023-12-07 12:03:39 +0900

    merge play_field.js & game.js

--- app/src/public/js/game/game.js
+++ app/src/public/js/game/game.js
@@ -3,6 +3,13 @@
 
 import getRandomNum from './randomizer.js';
 
+const canvas = document.getElementById('game-field');
+const ctx = canvas.getContext('2d');
+const squareSizeX = canvas.width / 10;
+const squareSizeY = canvas.height / 20;
+
+console.log(ctx, squareSizeX, squareSizeY);
+
 console.log(getRandomNum(7));
 console.log(getRandomNum(7));
 console.log(getRandomNum(7));
