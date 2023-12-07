ef1c0f0e18e124d7020c60f3a37e7eb84416f1c7
LC-02s <cl700120@gmail.com>
2023-12-07 13:04:19 +0900

    add loop module files

--- app/src/public/js/game/game.js
+++ app/src/public/js/game/game.js
@@ -1,7 +1,8 @@
 /* eslint-disable import/extensions */
 /* eslint-disable node/no-unsupported-features/es-syntax */
 
-import getRandomNum from './randomizer.js';
+import getRandomNum from './loop/logic/randomizer.js';
+import { shapes } from './variables.js';
 
 const canvas = document.getElementById('game-field');
 const ctx = canvas.getContext('2d');
@@ -10,6 +11,6 @@ const squareSizeY = canvas.height / 20;
 
 console.log(ctx, squareSizeX, squareSizeY);
 
-console.log(getRandomNum(7));
-console.log(getRandomNum(7));
-console.log(getRandomNum(7));
+console.log(getRandomNum(shapes.length));
+console.log(getRandomNum(shapes.length));
+console.log(getRandomNum(shapes.length));
